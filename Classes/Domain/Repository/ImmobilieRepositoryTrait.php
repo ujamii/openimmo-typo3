7360189758e0d6f7b8045f2195b7d179738dac5b
Michael Grundkötter <mg@ujamii.com>
2019-02-25 18:11:21 +0100

    [FEATURE] TYPO3 7.6 compatible version of repository

--- Classes/Domain/Repository/ImmobilieRepositoryTrait.php
+++ Classes/Domain/Repository/ImmobilieRepositoryTrait.php
@@ -1,12 +1,7 @@
 <?php
 namespace Ujamii\OpenImmoTypo3\Domain\Repository;
 
-use TYPO3\CMS\Core\Database\ConnectionPool;
-use TYPO3\CMS\Extbase\Persistence\Generic\Mapper\DataMapper;
 use Ujamii\OpenImmoTypo3\Domain\Dto\RealEstateSearchDemand;
-use Ujamii\OpenImmoTypo3\Domain\Model\Flaechen;
-use Ujamii\OpenImmoTypo3\Domain\Model\Immobilie;
-use Ujamii\OpenImmoTypo3\Domain\Model\Preise;
 
 /**
  */
@@ -16,42 +11,37 @@ trait ImmobilieRepositoryTrait {
      * @param RealEstateSearchDemand $filter
      *
      * @return array
+     * @throws \TYPO3\CMS\Extbase\Persistence\Exception\InvalidQueryException
      */
     public function findAllByFilter(RealEstateSearchDemand $filter)
     {
-        /* @var $pool ConnectionPool */
-        $pool = $this->objectManager->get(ConnectionPool::class);
-        $qb = $pool->getQueryBuilderForTable(Immobilie::TABLE_NAME);
-        $qb
-            ->select('imo.*')
-            ->from(Immobilie::TABLE_NAME, 'imo')
-            ->join('imo', Flaechen::TABLE_NAME, 'flaechen', 'flaechen.immobilie = imo.uid')
-            ->join('imo', Preise::TABLE_NAME, 'preise', 'preise.immobilie = imo.uid')
-        ;
+        $query = $this->createQuery();
+
+        $constraints = [];
 
         if ($filter->getNumberOfRoomsMin() != 0) {
-            $qb->andWhere($qb->expr()->gte('flaechen.anzahl_zimmer', $filter->getNumberOfRoomsMin()));
+            $constraints[] = $query->greaterThanOrEqual('flaechen.anzahl_zimmer', $filter->getNumberOfRoomsMin());
         }
         if ($filter->getNumberOfRoomsMax() != 0) {
-            $qb->andWhere($qb->expr()->lte('flaechen.anzahl_zimmer', $filter->getNumberOfRoomsMax()));
+            $constraints[] = $query->lessThanOrEqual('flaechen.anzahl_zimmer', $filter->getNumberOfRoomsMax());
         }
         if ($filter->getLivingAreaMin() != 0) {
-            $qb->andWhere($qb->expr()->gte('flaechen.wohnflaeche', $filter->getLivingAreaMin()));
+            $constraints[] = $query->greaterThanOrEqual('flaechen.wohnflaeche', $filter->getLivingAreaMin());
         }
         if ($filter->getLivingAreaMax() != 0) {
-            $qb->andWhere($qb->expr()->lte('flaechen.wohnflaeche', $filter->getLivingAreaMax()));
+            $constraints[] = $query->lessThanOrEqual('flaechen.wohnflaeche', $filter->getLivingAreaMax());
         }
         if ($filter->getPriceMin() != 0) {
-            $qb->andWhere($qb->expr()->gte('preise.warmmiete', $filter->getPriceMin()));
+            $constraints[] = $query->greaterThanOrEqual('preise.warmmiete', $filter->getPriceMin());
         }
         if ($filter->getPriceMax() != 0) {
-            $qb->andWhere($qb->expr()->lte('preise.warmmiete', $filter->getPriceMax()));
+            $constraints[] = $query->lessThanOrEqual('preise.warmmiete', $filter->getPriceMax());
         }
-        $qb->orderBy($filter->getSortBy());
 
-        /* @var $dataMapper DataMapper */
-        $dataMapper = $this->objectManager->get(DataMapper::class);
-        return $dataMapper->map(Immobilie::class, $qb->execute()->fetchAll());
+        $query->matching($query->logicalAnd($constraints));
+        $query->setOrderings([$filter->getSortBy() => QueryInterface::ORDER_ASCENDING]);
+
+        return $query->execute();
     }
 
 }
