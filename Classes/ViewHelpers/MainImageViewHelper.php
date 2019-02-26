29282534d2e9018ed696d0ef2dbf7a120e7d2184
Michael Grundkötter <mg@ujamii.com>
2019-02-26 11:46:45 +0100

    [FEATURE] added view helper for main image

---
+++ Classes/ViewHelpers/MainImageViewHelper.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Ujamii\OpenImmoTypo3\ViewHelpers;
+
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use Ujamii\OpenImmoTypo3\Domain\Model\Anhang;
+use Ujamii\OpenImmoTypo3\Domain\Model\Immobilie;
+
+/**
+ * Class MainImageViewHelper
+ * @package Ujamii\OpenImmoTypo3\ViewHelpers
+ */
+class MainImageViewHelper extends AbstractViewHelper {
+
+    /**
+     *
+     */
+    public function initializeArguments()
+    {
+        $this->registerArgument('immobilie', Immobilie::class, 'Object to return main image for.', true);
+    }
+
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     *
+     * @return mixed|Anhang|null
+     */
+    public static function renderStatic(array $arguments, ?\Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        $mainImage = null;
+
+        /* @var $immobilie Immobilie */
+        $immobilie = $arguments['immobilie'];
+
+        /* @var $anhang Anhang */
+        foreach ($immobilie->getAnhaenge()->getAnhang() as $anhang) {
+            if ($anhang->getGruppe() == Anhang::GRUPPE_TITELBILD) {
+                $mainImage = $anhang;
+                break;
+            } else if (is_null($mainImage) && $anhang->getGruppe() == Anhang::GRUPPE_BILD) {
+                $mainImage = $anhang;
+                // no break here, we may find a GRUPPE_TITELBILD later
+            }
+        }
+
+        return $mainImage;
+    }
+
+    /**
+     * v7 compat
+     *
+     * @return mixed|Anhang|null
+     * @deprecated
+     */
+    public function render()
+    {
+        return self::renderStatic($this->arguments, $this->renderChildrenClosure, $this->renderingContext);
+    }
+
+}
